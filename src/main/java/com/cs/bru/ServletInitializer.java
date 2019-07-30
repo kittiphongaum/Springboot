05463447a95cdd8e561038eed272254b004dd5b7
jook <kittiphong95@gmail.com>
2019-07-30 15:52:05 +0700

    Initial commit

---
+++ src/main/java/com/cs/bru/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cs.bru;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootApplication.class);
+	}
+
+}
---
+++ src/main/java/com/cs/bru/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.cs.bru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
